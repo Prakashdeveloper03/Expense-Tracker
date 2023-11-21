5a7bf25e72225da456f9ef8eae9260e85925d6c6
Siva Prakash <prakashdeveloper@outlook.in>
2023-11-21 09:56:49 +0530

    Generated components & services

---
+++ src/app/addtransaction/addtransaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-addtransaction',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './addtransaction.component.html',
+  styles: ``,
+})
+export class AddtransactionComponent {}
---
+++ src/app/incomeexpence/incomeexpence.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-incomeexpence',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './incomeexpence.component.html',
+  styles: ``,
+})
+export class IncomeexpenceComponent {}
